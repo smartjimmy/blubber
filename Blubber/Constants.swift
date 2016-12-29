1b9007269ad1e8bb0b20372877ede448d3d3272f
James Kang <jimmyjameskang@gmail.com>
2016-12-28 18:38:44 -0800

    sign in ui customer classes

---
+++ Blubber/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Blubber
+//
+//  Created by James Kang on 12/26/16.
+//  Copyright © 2016 James Kang. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ Blubber/FancyField.swift
@@ -0,0 +1,28 @@
+//
+//  FancyField.swift
+//  Blubber
+//
+//  Created by James Kang on 12/28/16.
+//  Copyright © 2016 James Kang. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+}
