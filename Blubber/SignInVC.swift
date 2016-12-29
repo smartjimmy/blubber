b122355e216a1e6e6e85d5d3a96e5133053bde90
James Kang <jimmyjameskang@gmail.com>
2016-12-29 11:49:45 -0800

    Facebook/Firebase Auth Setup done

--- Blubber/SignInVC.swift
+++ Blubber/SignInVC.swift
@@ -7,20 +7,48 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print("Say Hello World")
+
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("JIMMY: Unable to Authenticate with Facebook")
+            } else if result?.isCancelled == true {
+                print("JIMMY: User Cancelled Facebook Authentification")
+            } else {
+                print ("JIMMY: Successfuklly authentificated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+            
+        }
+    }
 
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("JIMMY: Unable to Authenticate with Firebase - \(error)")
+            } else {
+                print("JIMMY: Successfully authenticated with Firebase")
+            }
+        })
+    }
+    
 }
 
